5eda8c632966a0e75cd72b530f1ac2d573e06793
Mattia-Scaffidi <116498876+Mattia-Scaffidi@users.noreply.github.com>
2022-11-11 18:44:07 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,9 +4,17 @@ using namespace std;
 int main(){
 //Qui vanno definite le variabili
 
+float a=0;
+float b=0;
+float c=0;
 cin >> a >> b >> c;
+
 
 //Qui va scritto il codice per eseguire i calcoli richiesti
 
+cout<< (a*b)/2 <<endl;
+cout<< (a*a) <<endl;
+cout<< (a*b) <<endl;
+cout<< (((a+b)*c)/2) <<endl;
 
 }
